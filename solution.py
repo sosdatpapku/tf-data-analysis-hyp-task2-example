209825589f795c0052d4edd15e021fea01fa5a15
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-04-13 22:36:49 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from scipy.stats import ks_2samp
 chat_id = 1188007817 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    statistic, pvalue = ks_2samp(x, y, alternative='less')
+    statistic, pvalue = ks_2samp(x, y, alternative='two-sided')
     if pvalue < 0.08:
         return True
     else:
